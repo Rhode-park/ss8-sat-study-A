24021783e8b737305fc2bc17a8102dfca04374ec
minsu3007 <minsu3007@gmail.com>
2023-01-28 09:21:37 +0900

    feat: ContactInformation 프로토콜 구현

--- week6/StarterCamp-project/StarterCamp-project/ContactInformation.swift
+++ week6/StarterCamp-project/StarterCamp-project/ContactInformation.swift
@@ -1,7 +1,14 @@
 //  Contact.swift
 //  Created by 레옹아범 on 2022/11/18.
 
-struct ContactInformation {
+protocol ContactInformationProtocol {
+    var name: String { get }
+    var age: UInt { get }
+    var phoneNumber: String { get }
+    var fullText: String { get }
+}
+
+struct ContactInformation: ContactInformationProtocol {
     let name: String
     let age: UInt
     let phoneNumber: String
